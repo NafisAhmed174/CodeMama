16a0db6d428fe28f57ec0317df2de9d4335d699f
NafisAhmed174 <nafisahmed947@gmail.com>
2024-06-30 12:26:30 +0600

    added problem statement of sum of array elements

---
+++ JavaScript Problem Solving/Sum of Array Elements.js
@@ -0,0 +1,22 @@
+/*
+Problem Statement
+Write a program where user will give an array of elements. You will have to print the sum of elements where each element will be less than median element.
+
+Input
+The input consists of size of the array and the elements of the array. Every input will be unsigned integer number.
+
+Output
+The output will be sum of elements that are lesser than median element.
+
+Constraints
+0 ≤ |S| ≤ 109
+Example:
+Enter size of the array and the elements.
+
+Input:
+6
+30 10 5 40 60 15
+Output:
+30
+*/
+
